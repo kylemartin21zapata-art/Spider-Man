72c6aee87b5425345f12b15848672c7c95af55e7
Kyle Martin G. Zapata <kylemartin21zapata@gmail.com>
2026-09-22 22:56:59 +0800

    Modify

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const aboutBtn = document.getElementById("aboutBtn");
-const dropDown = document.getElementById("dropDown-Container")
+const dropDown = document.getElementById("dropDown-Container");
+const btnKnow = document.getElementById("btn-know");
+
 aboutBtn.addEventListener("click", function () {
-    dropDown.classList.toggle('active');
+  dropDown.classList.toggle("active");
+});
+
+btnKnow.addEventListener("click", function () {
+  document.getElementById("about").scrollIntoView({
+    behavior: "smooth",
+  });
 });
